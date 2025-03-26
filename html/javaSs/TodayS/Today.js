4685a192581c87f818ad6d4219f6f7c25978becb
Mouli <Moulishwar.k@navayuvabharatinfotech.com>
2025-03-26 18:29:07 +0530

    26/03/2025

---
+++ html/javaSs/TodayS/Today.js
@@ -0,0 +1,22 @@
+// ver let const
+
+ver A = The person is the use to the working
+
+
+let B = 2000
+
+
+const c = true
+
+
+
+console.log(A);
+
+console.log(B);
+
+
+console.log(C);
+
+console.log(A+B+C);
+
+
