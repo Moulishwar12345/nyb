3fc4a6b8ff1f5f9d773022761ae05e1a45a0e238
Mouli <Moulishwar.k@navayuvabharatinfotech.com>
2025-03-28 17:30:52 +0530

    28/03/2025

---
+++ html/javaSs/28/03/2025/today.js
@@ -0,0 +1,115 @@
+// Reference date type
+
+// Array
+
+// Object
+
+
+// function
+
+// es-6 map weak map set weak set
+
+
+
+// Array
+
+// array is the collection of values
+
+// collection of data (homogeneous and heterogeneous)
+
+
+
+// var apple = 'eating'
+
+// var ball = 'useing'
+
+// var cat = 'pet'
+
+// var A = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, apple, ball, 'cat', undefined, true, false, null, [10,20,30,40]]
+
+// var studDetales = ['mouli', 'sai', 'malli', 'teja', 'govardan']
+
+// console.log(studDetales[3]);
+
+
+// console.log(length.A);
+
+// console.log(A);
+
+
+// Object
+
+// mutiple values in single memory location
+
+
+var Empdetails = {
+
+    empname: "mouli",
+
+
+    empid: 420,
+
+    phno: 133456789,
+
+    email: 'moulki2gmail.com',
+
+    address : 'Atp',
+
+}
+
+
+var Empdetails1 = {
+
+    empname: "sai",
+
+
+    empid: 421,
+
+    phno: 133456789,
+
+    email: 'sai2gmail.com',
+
+    address : 'blg',
+
+}   
+
+
+var Empdetails2 = {
+
+    empname: "jay",
+
+
+    empid: 422,
+
+    phno: 133456789,
+
+    email: 'jay2gmail.com',
+
+    address : 'hyb',
+
+} 
+
+console.log(Empdetails);
+
+console.log(Empdetails.email);
+
+console.log(Empdetails["address"]);
+
+console.log(Empdetails1["address"]);
+
+console.log(Empdetails2.email);
+
+console.log(Empdetails1);
+
+console.log(Empdetails1.email);
+
+console.log(Empdetails2);
+
+console.log(Empdetails2.email);
+
+
+
+
+
+
+
