b1784ab5694e33f40e322f9aa3ebc5eccb2c765d
Mouli <Moulishwar.k@navayuvabharatinfotech.com>
2025-04-01 18:14:07 +0530

    01/04/2025

---
+++ html/javaSs/01/today.js
@@ -0,0 +1,106 @@
+// function
+
+// it is the block of the code to do some task and return some value 
+
+// function 
+
+// syntax 
+
+// function (keyword) function_Name(user choice)(parameters){
+
+// }
+
+console.log("Function");
+
+var A = 10
+
+function Hello() {
+    //code
+
+    var a = 525
+
+    var b = 4155
+
+
+    return "we are useing to the return statment of the work"
+
+
+
+
+    console.log(a);
+    console.log(b);
+    
+    
+}
+
+console.log(Hello());
+  //function is calling
+
+
+// Named function expression
+
+var hello=function Hello() {
+    //code
+   console.log("Named Function");
+   
+    var a = 252
+
+    var b = 525
+
+    console.log(a);
+    console.log(b);
+    
+    
+}
+
+hello() 
+
+
+
+
+// Annnomous Function Expression
+
+var m=function Hello() {
+    //code
+   console.log("Named Function");
+   
+    var a = 252
+
+    var b = 525
+
+    console.log(a);
+    console.log(b);
+    
+    
+}
+
+m() 
+
+
+// Arrow function 
+
+
+var m=()=> {
+    //code
+   console.log("Named Function");
+   
+    var a = 252
+
+    var b = 525
+
+    console.log(a);
+    console.log(b);
+    
+    
+}
+
+m() 
+
+
+
+
+
+
+
+
+
